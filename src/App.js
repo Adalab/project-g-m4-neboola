4a0f571da40e3721510e6c7c11512363e82afa30
martagrio <marta.igr90@gmail.com>
2019-09-30 11:49:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="app">
+      </div>
+    );
+  }
+}
+
+export default App;
