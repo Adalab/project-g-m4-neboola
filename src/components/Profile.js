67b18eb54bba426803042f0340bb803d178fbce4
neired <nmillans91@gmail.com>
2019-10-01 19:29:11 +0200

    add all icons and imgs

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,10 @@ import React from 'react';
 //import PropTypes from 'prop-types';
 //import Info from './Info';
 //import NewRequest from './NewRequest';
+import logout from './../images/logout.png';
+import profile from './../images/profile.png';
+import umbrella from './../images/umbrella.png';
+import check from './../images/check.png';
 import { Link } from 'react-router-dom';
 
 const Profile = props => {
@@ -10,20 +14,23 @@ const Profile = props => {
 		<React.Fragment>
       <header className="prof_header">
         <h1 className="prof_title">Neboola</h1>
+        <img src={logout} alt="Logout icon" className="prof_logout-icon"></img>
       </header>
       <main className="prof_main">
         <div className="prof_container">
-          <img src="" alt="" className="prof_img"/>
+          <img src={profile} alt="" className="prof_img"/>
           <h2 className="prof_user-name">Judith Elliott</h2>
           <p className="prof_mail">{data.email}</p>
-          <Link to="/newRequest" className="newrequest_link">
-            Request free time
+          <Link to="/newRequest" className="link newrequest_link">
+            <p className="newrequest_link_text">Request free time</p>
           </Link>
         </div>
         <div className="info_container-nav">
           <Link to="/info" className="link info_link">
             <div className="prof-info_container">
-              {/*<i></i> */}
+            <div className="prof_icon-container">
+              <img className="prof_icon" src={umbrella} alt="Umbrella icon"></img>
+            </div>
               <p className="prof_number">{data.remainingDays}</p>
               <p className="prof_text">available days</p>
             </div>
@@ -31,7 +38,9 @@ const Profile = props => {
           
           <Link to="/info" className="link info_link">
             <div className="prof-info_container">
-              {/*<i></i> */}
+              <div className="prof_icon-container">
+                <img className="prof_icon" src={check} alt="Check icon"></img>
+              </div>
               <p className="prof_number">{data.requestedDays}</p>
               <p className="prof_text">scheduled days</p>
             </div>
