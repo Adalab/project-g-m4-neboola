0f725a21a712e771f3c48c6914ea716c1799cabb
martagrio <marta.igr90@gmail.com>
2019-10-01 16:47:47 +0200

    Get back to initial content

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,34 +1,49 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import Error from './Error';
 import { Link } from 'react-router-dom';
 
 const Login = props => {
-	return(
+	const domain = 'neboola.co';
+	const { email, getEmail } = props;
+	return (
 		<React.Fragment>
-      <header className="app_header">
-          <h1 className="header_title">neboola</h1>
-        </header>
+			<header className="app_header">
+				<h1 className="header_title">neboola</h1>
+			</header>
 
-      <main className="app_main">
-		    <h2 className="main_title">Vacacioonero</h2>
-        <label htmlFor="" className="label_login">
-          <input type="mail" className="input_mail"/>
-        </label>
+			<main className="app_main">
+				<h2 className="main_title">Vacacioonero</h2>
+				<form action="" className="form_login" id="login">
+					<label htmlFor="" className="label_login">
+						<input type="email" className="input_email"
+							onChange={getEmail}
+							required
+							value={email} />
+					</label>
+				</form>
 
-        <Link to="/profile" className="profile_link">
-        <button type="submit" className="btn_login">Enter</button>
-        </Link>
+				{if(email.toLowerCase().includes(domain.toLowerCase())){
+					<Link to="/profile" className="profile_link">
+						<input type="submit" className="btn_login" form="login" value="enter" />
+					</Link>
+				} else {
+					<input type="submit" className="btn_login" form="login" value="enter" />
+				}
 
-		    <Error/>
+				{/* 				<Link to="/profile" className="profile_link">
+					<input type="submit" className="btn_login" form="login" value="enter"/>
+				</Link>
+		    <Error/> */}
+				}
       </main>
 		</React.Fragment>
-
 	);
 }
 
 Login.propTypes = {
-
+	getEmail: PropTypes.func.isRequired,
+	email: PropTypes.string.isRequired
 }
 
 export default Login;
