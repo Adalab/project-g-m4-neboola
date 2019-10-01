413cd4fd68bac8f878de325a8872794221b0964c
martagrio <marta.igr90@gmail.com>
2019-10-01 18:28:37 +0200

    Add validator @neboola.co

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,13 +4,13 @@ import Error from './Error';
 import { Link } from 'react-router-dom';
 
 const Login = props => {
-
+	const domain = '@neboola.co';
 	const {email, getEmail, getFetch} = props;
 
 	return(
 		<div className="login_container">
       <header className="app_header">
-          <h1 className="header_title">n</h1>
+          <h1 className="header_title">neboola</h1>
         </header>
 
       <main className="app_main">
@@ -23,7 +23,9 @@ const Login = props => {
 						value={email}/>
         </label>
         </form>
-        <Link to="/profile" className="profile_link">
+	       <Link to={email.toLowerCase().includes(domain.toLowerCase()) 
+					? '/profile' : '/'}  
+					className="profile_link">
          <input  type="submit" className="btn_login"  
             form="login"
             value="Enter"
