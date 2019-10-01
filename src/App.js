cca208e7a370c5232af7a4b16bc89a4edc9a5ec2
martagrio <marta.igr90@gmail.com>
2019-10-01 12:50:10 +0200

    Check fetch is working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-  }
 
+		this.state = {
+			data: {},
+			email: ''
+		}
+
+		this.getEmail = this.getEmail.bind(this);
+  }
+	
+	getEmail(event) {
+		const newEmail = event.currentTarget.value;
+		this.setState({
+			email: newEmail
+		});
+	}
+
+	componentDidMount() {
+		this.getFetch();
+	}
+
+	getFetch() {
+		const ENDPOINT = 'https://neboola-holidays-api.herokuapp.com/open/users/';
+		fetch(ENDPOINT + this.state.email)
+		.then(response =>response.json())
+		.then(data => console.log(data))
+		.catch(error => { console.log(error)});
+
+	}
   render() {
     return (
       <div className="app">
-				<Login />
+				<input type="text" className="text" onChange={this.getEmail} value={this.state.email}/>
       </div>
     );
   }
