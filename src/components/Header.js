9c82a72f0cf6073b5cf9411607ee797610d202fb
neired <nmillans91@gmail.com>
2019-10-02 13:56:35 +0200

    added styles to info and newrequest header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import logout from './../images/logout.png';
+import iconback from './../images/lelt-open-arrow.png';
 
 const Header = props => {
+  const {deleteLS} = props;
 	return(
 		<React.Fragment>
       <div className="header">
         <Link to="/profile" className="link profile_link">
-          <i className="header_icon-back"></i>
+        <img src={iconback} alt="Go back icon" className="header_icon-back"></img>
           <h1 className="header-logo">neboola</h1>
         </Link>
-        <Link to="/login" className="link logout_link">
-          <i className="logout_icon"></i>
+        <Link to="/" className="link" onClick={deleteLS}>
+          <img src={logout} alt="Logout icon" className="prof_logout-icon"></img>
         </Link>
+
+        <h1 className="prof_title">Neboola</h1>
+
       </div>
 		</React.Fragment>
 
