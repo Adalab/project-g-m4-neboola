5edec48b7f02f6d1f7f99c0b3b26fd5fa0e40719
Clara Brígido <clarabrigido@gmail.com>
2019-10-02 12:59:28 +0200

    Add list with dates (moments)

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,6 +7,9 @@ import Collapsibles from './Collapsibles';
 import Requests from './Requests';
  */
 
+import moment from "moment";
+
+
 class Info extends React.Component {
     render() {
         const { requests } = this.props;
@@ -16,10 +19,28 @@ class Info extends React.Component {
 						<Header/>
 							<h2 className="info_title">request time</h2>
 							<ul className="info_list">
+                <p>{`5 days  `}</p>
 								{requests.map(collapsible => {
 									return (
 										<li className="info_schedule" key={collapsible._id} id={collapsible._id}>
-											<div className="item_lists">{collapsible._id}</div>
+											<div className="item_lists">
+                        
+                        <img className="collapsible_icon" src="" alt=""/>
+                        <p className="create_date">{moment(collapsible.createdAt).format('DD/MM/YYYY')}</p> 
+                        <p className="create_count">{collapsible.daysCount} days</p> 
+                        <div className="boxes_date">
+                         <div className="date_container">
+                            <p className=" text text-from">from</p>
+                            <p className="date">{moment(collapsible.startDate).format('DD MMM YYYY')} </p>
+                          </div>
+
+                           <div className="date_container">
+                            <p className=" text text-to">to</p>
+                            <p className="date"> {moment(collapsible.endDate).format('DD MMM YYYY')}</p>
+                          </div>
+                          
+                        </div>
+                      </div>
 										</li>
 									)
 								})}
