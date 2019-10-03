57a7564617e698e32299c500c28e3b809ad180aa
martagrio <marta.igr90@gmail.com>
2019-10-03 15:14:55 +0200

    Run docs

---
+++ docs/precache-manifest.a13332e4113e5f224a0a9a285e34bd69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e874638fe4de0e00ce75de5bc98f315",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c475de6847761b93807",
+    "url": "./static/css/main.a6a59d78.chunk.css"
+  },
+  {
+    "revision": "7f280aa9e95c398702b3",
+    "url": "./static/js/2.9911667f.chunk.js"
+  },
+  {
+    "revision": "2c475de6847761b93807",
+    "url": "./static/js/main.d73a251d.chunk.js"
+  },
+  {
+    "revision": "e0eb988c53bf58fc8afa",
+    "url": "./static/js/runtime-main.672fe195.js"
+  },
+  {
+    "revision": "816c4c0f04f0878c4e2ff0c244ba16b0",
+    "url": "./static/media/HelveticaNeue.816c4c0f.ttc"
+  },
+  {
+    "revision": "104357fcce3fbe1498082a218e4fa0fb",
+    "url": "./static/media/logo.104357fc.svg"
+  },
+  {
+    "revision": "4fad9bc7623c72171c1b20e4edde5afe",
+    "url": "./static/media/profile.4fad9bc7.png"
+  }
+]);
